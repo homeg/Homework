16b81278339cf657b52b6ebd47f4cf8a6404e8ff
Vladimir Gavrylov <iboved@gmail.com>
2014-10-26 18:47:30 +0200

    Added Autoloader, change index.php

---
+++ Autoloader.php
@@ -0,0 +1,12 @@
+<?php
+class Autoloader
+{
+    public static function loadPackages($className)
+    {
+        $pathParts = explode('\\', $className);
+        $loadedFilename = implode(DIRECTORY_SEPARATOR, $pathParts) . '.php';
+        if (file_exists($loadedFilename)) {
+        require_once($loadedFilename);
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__.'/Autoloader.php';
+
+use Core\Rectangle;
+use Core\Circle;
